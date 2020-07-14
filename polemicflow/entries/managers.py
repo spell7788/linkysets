8895e1f5d6616d6b1c12f2ccdbf68bff454d6935
hqrrylyu <naytonmoranga@gmail.com>
2020-07-14 16:40:46 +0300

    :sparkles: The number of sets the entry is in annotation

--- polemicflow/entries/managers.py
+++ polemicflow/entries/managers.py
@@ -3,16 +3,18 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from django.apps import apps
-from django.db.models import Count, Manager, Prefetch, QuerySet
+from django.db.models import Count, Manager, OuterRef, Prefetch, QuerySet, Subquery
 
 if TYPE_CHECKING:
-    from .models import EntrySet
+    from .models import EntrySet, Entry
 
     EntrySetQuerySetBase = QuerySet[EntrySet]
     EntrySetManagerBase = Manager[EntrySet]
+    EntryManagerBase = Manager[Entry]
 else:
     EntrySetQuerySetBase = QuerySet
     EntrySetManagerBase = Manager
+    EntryManagerBase = Manager
 
 
 class EntrySetQuerySet(EntrySetQuerySetBase):
@@ -21,6 +23,19 @@ class EntrySetQuerySet(EntrySetQuerySetBase):
 
     def num_replies(self) -> EntrySetQuerySet[EntrySet]:
         return self.annotate(num_replies=Count("replies"))
+
+    def prefetch_entries(self) -> EntrySetQuerySet[EntrySet]:
+        Entry = apps.get_model("entries.Entry")
+        # Annotate entry sets count in prefetch subquery
+        # https://stackoverflow.com/a/44474268/10079612
+        m2m_model = Entry.sets.through
+        m2m_qs = m2m_model.objects.filter(entry=OuterRef("pk")).values("pk")
+        num_of_sets_qs = m2m_qs.annotate(num_of_sets=Count("*")).values("num_of_sets")
+        num_of_sets_qs.query.group_by = []
+
+        qs = Entry.objects.annotate(num_of_sets=Subquery(num_of_sets_qs))
+        qs = qs.select_related("origin")
+        return self.prefetch_related(Prefetch("entries", queryset=qs))
 
     def prefetch_replies(self) -> EntrySetQuerySet[EntrySet]:
         Reply = apps.get_model("replies.Reply")
@@ -33,5 +48,5 @@ class EntrySetManager(EntrySetManagerBase):
     def get_queryset(self) -> EntrySetQuerySet[EntrySet]:
         qs = super().get_queryset()
         qs = qs.select_related("author")
-        qs = qs.prefetch_related("entries")
+        qs = qs.prefetch_entries()
         return qs
