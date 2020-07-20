f99eb1ffbbd4e36d4e15e79afb019cc192ed27aa
hqrrylyu <naytonmoranga@gmail.com>
2020-07-20 17:43:32 +0300

    :sparkles: transform_query template tag prefix feature

--- polemicflow/common/templatetags/common_tags.py
+++ polemicflow/common/templatetags/common_tags.py
@@ -7,10 +7,17 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def transform_query(context: dict, safe: Optional[str] = None, **kwargs) -> str:
+def transform_query(
+    context: dict,
+    prefix: str = "",
+    prefix_sep: str = "_",
+    safe: Optional[str] = None,
+    **query_transform,
+) -> str:
     request: HttpRequest = context["request"]
     querydict = request.GET.copy()
-    for key, value in kwargs.items():
-        querydict[key] = value
+    for key, value in query_transform.items():
+        key_ = prefix_sep.join([prefix, key]) if prefix else key
+        querydict[key_] = value
 
     return querydict.urlencode(safe=safe)
--- polemicflow/common/tests/test_templatetags.py
+++ polemicflow/common/tests/test_templatetags.py
@@ -34,3 +34,45 @@ class TransformQueryTests(TestCase):
         ).render(Context({"request": self.request}, autoescape=False))
         # fmt: on
         self.assertEqual(out, f"{self.param_key}={new_value}")
+
+    def test_adds_prefixed_parameter(self):
+        prefix = fake.pystr()
+        # fmt: off
+        out = Template(
+            "{% load common_tags %}"
+            f"{{% transform_query prefix='{prefix}' "
+            f"{self.param_key}='{self.param_value}' %}}"
+        ).render(Context({"request": self.request}, autoescape=False))
+        # fmt: on
+        self.assertEqual(
+            out,
+            f"{self.param_key}={self.param_value}"
+            f"&{prefix}_{self.param_key}={self.param_value}",
+        )
+
+    def test_joins_with_custom_prefix_separator(self):
+        prefix = fake.pystr()
+        prefix_sep = "-"
+        # fmt: off
+        out = Template(
+            "{% load common_tags %}"
+            f"{{% transform_query prefix='{prefix}' prefix_sep='{prefix_sep}' "
+            f"{self.param_key}='{self.param_value}' %}}"
+        ).render(Context({"request": self.request}, autoescape=False))
+        # fmt: on
+        self.assertEqual(
+            out,
+            f"{self.param_key}={self.param_value}"
+            f"&{prefix}{prefix_sep}{self.param_key}={self.param_value}",
+        )
+
+    def test_does_not_encode_safe_characters(self):
+        safe = "/"
+        # fmt: off
+        out = Template(
+            "{% load common_tags %}"
+            f"{{% transform_query safe='{safe}' "
+            f"{self.param_key}='/{self.param_value}/' %}}"
+        ).render(Context({"request": self.request}, autoescape=False))
+        # fmt: on
+        self.assertEqual(out, f"{self.param_key}=/{self.param_value}/")
